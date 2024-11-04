0e4bc72464fef921677918b8f2a3c30c1110c463
Galaxy-104 <js_park95@naver.com>
2024-11-04 14:20:40 +0900

    feat : board 데이터 및 타입 설정

--- src/store/slices/boardsSlice.ts
+++ src/store/slices/boardsSlice.ts
@@ -1,8 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { IBoard } from "../../types";
 
-const initialState = {
+type TboardsState = {
+	modalActive: boolean,
+	boardArray: IBoard[],
+}
+
+const initialState: TboardsState = {
 	modalActive: false,
-	boardArray: []
+	boardArray: [
+		{
+			boardId: "board-0",
+			boardName: "첫 번째 게시물",
+			lists: [
+				{
+					listId: "list-0", 
+					listName: "list 1",
+					tasks: [
+						{
+							taskId: "task-0",
+							taskName: "Task 1",
+							taskDescription: "Description",
+							taskOwner: "J",
+						},
+						{
+							taskId:"task-1",
+							taskName: "Task 1",
+							taskDescription: "Description",
+							taskOwner: "J"
+						}
+					]
+				},
+				{
+					listId: "list-1",
+					listName: "List 2",
+					tasks: [
+						{
+							taskId: "task-3",
+							taskName: "Task 3",
+							taskDescription: "Description",
+							taskOwner: "J"
+						}
+					]
+				}
+			]
+		}
+	]
 }
 
 const boardSlice = createSlice({
