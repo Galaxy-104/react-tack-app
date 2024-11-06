83619ddf56ef31fa7b7fbf01d88906933c2bf154
Galaxy-104 <js_park95@naver.com>
2024-11-06 17:09:25 +0900

    feat : EditModal 컴포넌트 생성 및 스타일 적용

--- src/store/slices/boardsSlice.ts
+++ src/store/slices/boardsSlice.ts
@@ -26,6 +26,12 @@ type TAddTaskAction = {
 	task: ITask;
 }
 
+type TDeleteTaskAction = {
+	boardId: string;
+	listId: string;
+	taskId: string;
+}
+
 const initialState: TboardsState = {
 	modalActive: false,
 	boardArray: [
@@ -100,6 +106,49 @@ const boardSlice = createSlice({
 
 			)
 		},
+		updateTask: (state, {payload}: PayloadAction<TAddTaskAction>) => {
+			state.boardArray = state.boardArray.map(
+				board => 
+					board.boardId === payload.boardId ?
+					{
+						...board,
+						lists: board.lists.map(list =>
+							list.listId === payload.listId ?
+							{
+								...list,
+								tasks: list.tasks.map(task => 
+									task.taskId === payload.task.taskId ?
+									payload.task : task
+								)
+							}
+							:
+							list
+						)
+					}
+					:
+					board 
+			)
+		},
+		deleteTask: (state, {payload}: PayloadAction<TDeleteTaskAction>) => {
+			state.boardArray = state.boardArray.map(
+				board =>
+					board.boardId === payload.boardId ?
+					{
+						...board,
+						lists: board.lists.map( list => 
+							list.listId === payload.listId ?
+							{
+								...list,
+								tasks: list.tasks.filter(
+									task => task.taskId !== payload.taskId
+								)
+							}
+							: list
+						)
+					}
+					: board
+			)
+		},
 		deleteList: (state, {payload}: PayloadAction<TdeleteListAction>) => {
 			state.boardArray = state.boardArray.map(
 				board => 
@@ -120,5 +169,13 @@ const boardSlice = createSlice({
 	}
 })
 
-export const { addBoard, deleteList, setModalActive, addList, addTask } = boardSlice.actions;
+export const { 
+	addBoard, 
+	deleteList, 
+	setModalActive, 
+	addList, 
+	addTask,
+	updateTask,
+	deleteTask
+} = boardSlice.actions;
 export const boardsReducer = boardSlice.reducer;
